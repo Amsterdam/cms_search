f0a033f59de39c24602f2b885bf3042cd03cd0ef
royderks <10717410+royderks@users.noreply.github.com>
2019-11-25 11:57:39 +0100

    Changed the schema to be able to work with partial search and use a prefix for the last word in the query string

--- src/es/es.schema.ts
+++ src/es/es.schema.ts
@@ -4,56 +4,73 @@ export type ElasticSearchArgs = {
   q: string
 } & SearchInput
 
-export default ({ q, limit, from, types }: ElasticSearchArgs) => ({
-  query: {
-    bool: {
-      must: [],
-      must_not: [],
-      should: [
-        {
-          prefix: {
-            title: {
-              value: q,
-              boost: 4.0,
-            },
+export default ({ q, limit, from, types }: ElasticSearchArgs) => {
+  let terms = q.split(' ')
+  const nrTerms = terms.length
+
+  /** The way the results are indexed by the Drupal ElasticSearch plugin requires us to construct
+   * the ES shcema like this, where the query string is transformed to an array and all values
+   * from this array are searched as terms except for the last one - this value is
+   * prefixed to also find part of words
+   **/
+  const should = ['', ...terms].reduce((acc, cur, index): any => {
+    const queryType = nrTerms === index ? 'prefix' : 'term'
+
+    return [
+      ...acc,
+      {
+        [queryType]: {
+          title: {
+            value: cur,
+            boost: 4.0,
           },
         },
-        {
-          prefix: {
-            field_intro: {
-              value: q,
-              boost: 2.0,
-            },
-          },
-        },
-        {
-          prefix: {
-            processed: {
-              value: q,
-              boost: 1.0,
-            },
-          },
-        },
-      ],
-      filter: {
-        terms: { type: types },
       },
-      minimum_should_match: 1,
-    },
-  },
-  from,
-  size: limit,
-  sort: [
-    { field_publication_date: { order: 'desc' } },
-    { field_publication_year: { order: 'desc' } },
-    { field_publication_month: { order: 'desc' } },
-    '_score',
-  ],
-  aggs: {
-    count_by_type: {
-      terms: {
-        field: 'type',
+      {
+        term: {
+          field_intro: {
+            value: cur,
+            boost: 2.0,
+          },
+        },
+      },
+      {
+        term: {
+          processed: {
+            value: cur,
+            boost: 1.0,
+          },
+        },
+      },
+    ]
+  })
+
+  return {
+    query: {
+      bool: {
+        must: [],
+        must_not: [],
+        should: should,
+        filter: {
+          terms: { type: types },
+        },
+        minimum_should_match: nrTerms, // All the terms from the query string must be matched
       },
     },
-  },
-})
+    from,
+    size: limit,
+    sort: [
+      { field_publication_date: { order: 'desc' } },
+      { field_publication_year: { order: 'desc' } },
+      { field_publication_month: { order: 'desc' } },
+      '_score',
+    ],
+    aggs: {
+      count_by_type: {
+        terms: {
+          field: 'type',
+        },
+      },
+    },
+  }
+}
