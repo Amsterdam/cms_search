da346902c1b8390e67521bfadc62792ae3b8bce3
royderks <10717410+royderks@users.noreply.github.com>
2020-01-13 16:28:46 +0100

    Improve the relevancy of the search results returned by the cms search queries

--- src/es/cms/es.schema.ts
+++ src/es/cms/es.schema.ts
@@ -5,103 +5,91 @@ export type ElasticSearchArgs = {
 } & CmsSearchInput
 
 export default ({ q, limit, from, types, filters: themeFilters, sort }: ElasticSearchArgs) => {
-  const searchQuery = q.toLocaleLowerCase()
-  let terms = searchQuery.split(' ')
-  const nrTerms = terms.length
+  let should: Array<object> = []
+  let sorting: Array<object> = []
 
-  /** The way the results are indexed by the Drupal ElasticSearch plugin requires us to construct
-   * the ES shcema like this, where the query string is transformed to an array and all values
-   * from this array are searched as terms except for the last one - this value is
-   * prefixed to also find part of words
-   **/
-  // const should = ['', ...terms].reduce((acc, cur): any => {
-  //   return [
-  //     ...acc,
-  //     {
-  //       match: {
-  //         title: {
-  //           query: `*${cur}*`,
-  //           boost: 6.0,
-  //         },
-  //       },
-  //     },
-  //     {
-  //       term: {
-  //         field_intro: {
-  //           value: cur,
-  //           boost: 2.0,
-  //         },
-  //       },
-  //     },
-  //     {
-  //       term: {
-  //         processed: {
-  //           value: cur,
-  //           boost: 1.0,
-  //         },
-  //       },
-  //     },
-  //   ]
-  // })
+  if (q && q.length > 0) {
+    const searchQuery = q.toLowerCase()
+    const terms = searchQuery.split(' ')
+    const nrTerms = terms.length
 
-  const minimumShouldMatch = 2
-  const should = [
-    {
-      bool: {
-        should: ['', ...terms].reduce((acc, cur): any => {
-          return [
-            ...acc,
-            {
-              match: {
-                title: {
-                  query: `*${cur}*`,
-                  boost: 8.0,
-                },
+    /** The way the results are indexed by the Drupal ElasticSearch plugin requires us to construct
+     * the ES schema like this, where the query string is transformed to an array and all values
+     * from this array are searched. The value for "minimum_should_match" is set to 1, so at least
+     * one of the shoudl rules must match:
+     *
+     * 1) With one search term: the search term is used in the title OR in the intro
+     * 2) With multiple search terms: all search terms are used in the title OR at least one search
+     *    term is used in the title and at least one of the other search terms are used in the intro
+     **/
+    should = [
+      {
+        bool: {
+          // The term must be used in the title standalone, as prefix or as suffix
+          must: terms.map((term: string) => ({
+            wildcard: {
+              title: {
+                value: `*${term}*`,
+                boost: 2.0,
               },
             },
-          ]
-        }),
+          })),
+        },
       },
-    },
-    {
-      bool: {
-        should: ['', ...terms].reduce((acc, cur): any => {
-          return [
-            ...acc,
-            {
-              match: {
-                title: {
-                  query: `*${cur}*`,
-                  boost: 4.0,
+      ...terms.map((term: string) => {
+        // If there's only one search term, this term must be used inside the intro
+        if (nrTerms === 1) {
+          return {
+            bool: {
+              should: [
+                {
+                  term: {
+                    field_intro: {
+                      value: terms[0],
+                      boost: 1.0,
+                    },
+                  },
+                },
+              ],
+            },
+          }
+        }
+
+        // When there are multiple search terms at least one must match in the title, and at least one
+        // of the other search terms must match in the intro
+        return {
+          bool: {
+            minimum_should_match: nrTerms - 1,
+            must: [
+              {
+                wildcard: {
+                  title: {
+                    value: `*${term}*`,
+                    boost: 2.0,
+                  },
                 },
               },
-            },
-            {
-              term: {
-                field_intro: {
-                  value: cur,
-                  boost: 2.0,
-                },
-              },
-            },
-            {
-              term: {
-                processed: {
-                  value: cur,
-                  boost: 1.0,
-                },
-              },
-            },
-          ]
-        }),
-      },
-    },
-  ]
+            ],
+            should: terms
+              .filter(shouldTerm => shouldTerm !== term)
+              .map((shouldTerm: string) => {
+                return {
+                  term: {
+                    field_intro: {
+                      value: shouldTerm,
+                      boost: 1.0,
+                    },
+                  },
+                }
+              }),
+          },
+        }
+      }),
+    ]
+  }
 
   const filters =
     themeFilters && themeFilters.map(themeFilter => ({ term: { field_theme_id: themeFilter } }))
-
-  let sorting: Array<object> = []
 
   if (sort) {
     const { order } = sort
@@ -145,7 +133,7 @@ export default ({ q, limit, from, types, filters: themeFilters, sort }: ElasticS
             type: types,
           },
         },
-        minimum_should_match: minimumShouldMatch, // All the terms from the query string must be matched
+        minimum_should_match: should.length > 0 ? 1 : 0, // At least one of the should rules must match
       },
     },
     from,
