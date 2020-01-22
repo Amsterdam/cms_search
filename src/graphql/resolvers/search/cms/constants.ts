73401596666b609db41af2671590080c9134930e
royderks <10717410+royderks@users.noreply.github.com>
2020-01-22 11:51:10 +0100

    Add filtertypes

--- src/graphql/resolvers/search/cms/constants.ts
+++ src/graphql/resolvers/search/cms/constants.ts
@@ -1,4 +1,10 @@
+export const FILTER_TYPES = {
+  RADIO: 'radio',
+  CHECKBOX: 'checkbox',
+  SELECT: 'select',
+}
+
 export const FILTERS = {
-  THEME: { type: 'theme', label: "Thema's", filterType: 'array' },
-  DATE: { type: 'date', label: 'Publicatiedatum', filterType: 'string' },
+  THEME: { type: 'theme', label: "Thema's", filterType: FILTER_TYPES.CHECKBOX },
+  DATE: { type: 'date', label: 'Publicatiedatum', filterType: FILTER_TYPES.SELECT },
 }
