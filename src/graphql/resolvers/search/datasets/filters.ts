938fa00e56e53118b578964ac814b979c249a11b
Chris van Mook <chris@chrisvanmook.com>
2020-01-22 11:52:14 +0100

    Using new environment vars

--- src/graphql/resolvers/search/datasets/filters.ts
+++ src/graphql/resolvers/search/datasets/filters.ts
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch'
-import { formatFilters, getCatalogFilters } from '../datasets/normalize'
+import { formatFilters, getCatalogFilters } from './normalize'
 import withCache from '../../../utils/memoryCache'
+
+export const DCAT_ENDPOINTS = JSON.parse(process.env.DCAT_ENDPOINTS || '')
 
 const week = 60 * 60 * 24 * 7
 
 export const openApiCached = async () =>
-  withCache('openApi', fetch(`${process.env.DATAPUNT_API_URL}dcatd/openapi`), week)
+  withCache('openApi', fetch(DCAT_ENDPOINTS['openapi']), week)
 
 export default async (_: any, { q }: any): Promise<any> => {
   let filters
