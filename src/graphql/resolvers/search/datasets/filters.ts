6fa70c21d1a6be6d75f37327b4846750d2dcae05
royderks <10717410+royderks@users.noreply.github.com>
2020-01-23 15:16:55 +0100

    Send an empty query string to the openapi when no searchterm has been entered

--- src/graphql/resolvers/search/datasets/filters.ts
+++ src/graphql/resolvers/search/datasets/filters.ts
@@ -9,7 +9,7 @@ const week = 60 * 60 * 24 * 7
 export const openApiCached = async () =>
   withCache('openApi', fetch(DCAT_ENDPOINTS['openapi']), week)
 
-export default async (_: any, { q }: any): Promise<any> => {
+export default async (_: any, { q = '' }: any): Promise<any> => {
   let filters
   try {
     const urlQuery = new URLSearchParams({ q }).toString()
