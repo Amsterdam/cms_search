be27711cbf30749424e6c7b9ee4cd4663c2d6b0e
Thomas Mills <mills.thoma@gmail.com>
2021-12-28 14:07:36 +0100

    Disable rate limiting

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,9 @@ import { isDevelopment } from './utils/environment'
 import express from 'express'
 import expressPlayground from 'graphql-playground-middleware-express'
 import cors from 'cors'
-import rateLimit from 'express-rate-limit'
 import { PORT, URL_PREFIX } from './config'
 import GraphQLMiddleware from './graphql'
 import TypeAheadMiddleWare from './typeahead'
-
-// For now, it's faster to add rate limiting on express level instead of adding nginx and configuring the Jenkinsfile properly,
-// as we will need the assistance of team Basis.
-// In the future we should add nginx because it's faster and better at handling things like rate-limiting
-
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 100, // limit each IP to 50 requests per windowMs
-})
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -47,7 +37,6 @@ app.use(
     callback(null, { origin: originHeader && allowedDomains.indexOf(originHeader) !== -1 })
   }),
 )
-app.use(limiter)
 
 // Health check
 app.get('/metrics', (req, res) => res.send('Working!')) // Internal
