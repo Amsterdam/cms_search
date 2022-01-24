b59ec8163a792cae23e647532d9bf66a06438527
Thomas Mills <mills.thoma@gmail.com>
2022-01-24 16:05:41 +0100

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 require('dotenv').config()
 
 import * as Sentry from '@sentry/node'
-import { isDevelopment, isProduction } from './utils/environment'
+import { isDevelopment } from './utils/environment'
 import express from 'express'
 import expressPlayground from 'graphql-playground-middleware-express'
 import cors from 'cors'
